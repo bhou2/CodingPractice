aa2fe1f1489eb5620dc0482d212172a9fdcbb22d
Binbing Hou <houbinbing.cs.hust@gmail.com>
2018-05-24 17:10:52 -0500

    Create List-Reverse.py

---
+++ List-Reverse.py
@@ -0,0 +1,35 @@
+# Iterative method
+
+def reverseList(list):
+    
+      #Initializing values
+       prev = None
+       curr = list.head
+       nex = curr.getNextNode()
+  
+       #looping
+       while curr:
+           #reversing the link
+           curr.setNextNode(prev)     
+
+           #moving to next node      
+           prev = curr
+           curr = nex
+           if nex:
+               nex = nex.getNextNode()
+
+       #initializing head
+       list.head = prev
+
+
+# Recursive method
+
+def reverse(self,node):
+
+       if node.getNextNode() == None:
+           self.head = node
+           return
+       self.reverse(node.getNextNode())
+       temp = node.getNextNode()
+       temp.setNextNode(node)
+       node.setNextNode(None)
