61177be7c27d91c1d1fd38461e9dae498e5a6b74
Binbing Hou <houbinbing.cs.hust@gmail.com>
2018-05-24 17:12:37 -0500

    Update List-Reverse.py

--- List-Reverse.py
+++ List-Reverse.py
@@ -2,34 +2,34 @@
 
 def reverseList(list):
     
-      #Initializing values
-       prev = None
-       curr = list.head
-       nex = curr.getNextNode()
+    #Initializing values
+    prev = None
+    curr = list.head
+    nex = curr.getNextNode()
   
-       #looping
-       while curr:
-           #reversing the link
-           curr.setNextNode(prev)     
+    #looping
+    while curr:
+        #reversing the link
+        curr.setNextNode(prev)     
 
-           #moving to next node      
-           prev = curr
-           curr = nex
-           if nex:
-               nex = nex.getNextNode()
+        #moving to next node      
+        prev = curr
+        curr = nex
+        if nex:
+            nex = nex.getNextNode()
 
-       #initializing head
-       list.head = prev
+    #initializing head
+    list.head = prev
 
 
 # Recursive method
 
 def reverse(self,node):
 
-       if node.getNextNode() == None:
-           self.head = node
-           return
-       self.reverse(node.getNextNode())
-       temp = node.getNextNode()
-       temp.setNextNode(node)
-       node.setNextNode(None)
+    if node.getNextNode() == None:
+         self.head = node
+         return
+    self.reverse(node.getNextNode())
+    temp = node.getNextNode()
+    temp.setNextNode(node)
+    node.setNextNode(None)
